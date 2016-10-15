26e450fc4a6d5b0871eaf44cf3f43c610e101929
Untref <Untref@Untref-PC>
2016-10-15 11:51:16 -0300

    Refactoring de crearColectaConUnMontoPositivoARecaudar

--- src/ejercicios/Colecta.java
+++ src/ejercicios/Colecta.java
@@ -4,11 +4,18 @@ public class Colecta {
 
 	private double objetivo;
 
+	/**
+	 * post: Colecta lista para recaudar el objetivo indicado
+	 *       en 'montoObjetivo'.
+	 */
 	public Colecta(double montoObjetivo) {
 
 		this.objetivo = montoObjetivo;
 	}
 
+	/**
+	 * post: devuelve el monto que representa el objetivo a recaudar.
+	 */
 	public double obtenerObjetivo() {
 		
 		return this.objetivo;
