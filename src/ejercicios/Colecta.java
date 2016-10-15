08923fbb2955efcb7e8cd2013c48549ca5028e99
Untref <Untref@Untref-PC>
2016-10-15 11:54:05 -0300

    Implementación de crearColectaConUnMontoNegativoGeneraUnError

--- src/ejercicios/Colecta.java
+++ src/ejercicios/Colecta.java
@@ -5,11 +5,18 @@ public class Colecta {
 	private double objetivo;
 
 	/**
+	 * pre : 'montoObjetivo' debe ser un valor positivo, en [$].
 	 * post: Colecta lista para recaudar el objetivo indicado
 	 *       en 'montoObjetivo'.
 	 */
 	public Colecta(double montoObjetivo) {
 
+		if (montoObjetivo < 0) {
+			
+			Error montoNegativo = new Error("El monto no puede ser negativo");
+			throw montoNegativo;
+		}
+		
 		this.objetivo = montoObjetivo;
 	}
 
